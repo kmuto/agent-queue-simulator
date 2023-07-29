e7b52b0c78839a0ab3dfa8e4ca64eaec12318e2b
Kenshi Muto <kmuto@kmuto.jp>
2023-07-29 22:26:14 +0900

    tick

--- gover/a.go
+++ gover/a.go
@@ -5,24 +5,32 @@ import (
 	"time"
 )
 
-// 30秒単位での時間を黙々と返す
-func ticks(ticksChan chan<- int64, from time.Time, to time.Time) {
-	defer close(ticksChan)
-	from_i := from.Unix()
-	to_i := to.Unix()
-	for i := from_i; i < to_i; i += 60 {
-		ticksChan <- i
-		ticksChan <- i + 30
-	}
+type loopState uint8
+
+const (
+	loopStateFirst loopState = iota
+	loopStateDefault
+	loopStateQueued
+	loopStateHadError
+	loopStateTerminating
+)
+
+type postValue struct {
+	values   []string
+	retryCnt int
 }
 
-// 障害時間配列を作る
+func newPostValue(values []string) *postValue {
+	return &postValue{values, 0}
+}
+
+// 障害時間配列を作る。30秒ごとにダウンタイムのときはtrueにする
 func makeDownTimeMap() map[int64]bool {
 	downTimeMap := make(map[int64]bool)
-	from := time.Date(2023, 7, 28, 10, 15, 0, 0, time.Local).Unix()
-	to := time.Date(2023, 7, 28, 10, 30, 0, 0, time.Local).Unix()
-	for i := from; i < to; i++ {
-		downTimeMap[i] = true
+	from_t := time.Date(2023, 7, 28, 10, 15, 0, 0, time.Local).Unix()
+	to_t := time.Date(2023, 7, 28, 10, 30, 0, 0, time.Local).Unix()
+	for t := from_t; t < to_t; t += 30 {
+		downTimeMap[t] = true
 	}
 	return downTimeMap
 }
@@ -31,37 +39,57 @@ func postMetric(queue chan<- string, tick int64) {
 	queue <- fmt.Sprintf("%v", time.Unix(tick, 0))
 }
 
-func mainLoop(ticksChan <-chan int64, queue chan string) {
-	downTimeMap := makeDownTimeMap()
-	for i := range ticksChan {
-		if i%60 == 0 {
-			if downTimeMap[i] {
-				fmt.Print("!!")
-			}
-			postMetric(queue, i)
-		}
+// 30秒単位での時間を黙々と返す
+func ticks(ticksChan chan<- int64, from time.Time, to time.Time, termCh chan<- struct{}) {
+	from_t := from.Unix()
+	to_t := to.Unix()
+	for t := from_t; t < to_t; t += 60 {
+		ticksChan <- t
+		ticksChan <- t + 30
 	}
-
-	for data := range queue {
-		fmt.Println(data)
-	}
-	close(queue)
+	termCh <- struct{}{}
 }
 
 func main() {
 	from := time.Date(2023, 7, 28, 10, 0, 0, 0, time.Local)
 	to := time.Date(2023, 7, 28, 11, 0, 0, 0, time.Local)
-	ticksChan := make(chan int64)
+	ticksCh := make(chan int64)
 	downTimeMap := makeDownTimeMap()
 
-	go ticks(ticksChan, from, to)
-	for i := range ticksChan {
+	queue := make(chan string, 3)
+	termCh := make(chan struct{})
+
+	go ticks(ticksCh, from, to, termCh)
+
+	for {
+		select {
+		case t := <-ticksCh:
+			if downTimeMap[t] {
+				fmt.Print("! ")
+			}
+			if t%60 == 0 {
+				queue <- fmt.Sprintf("%v", time.Unix(t, 0))
+			}
+			fmt.Println(t)
+		case v := <-queue:
+			fmt.Println("POSTED at ", v)
+		case <-termCh:
+			fmt.Println("EXIT")
+			return
+		}
+	}
+	//go ticks(timerPost, from, to)
+	/*for i := range timerReceive {
 		if i%60 == 0 {
 			if downTimeMap[i] {
 				fmt.Println("!!", time.Unix(i, 0))
 			}
+			// 投稿
+			//			queue <- fmt.Sprintf("%v", time.Unix(i, 0))
 		}
-	}
+	}*/
+
+	//fmt.Println(len(queue))
 
 	/*	select {
 		case <-ticksChan:
